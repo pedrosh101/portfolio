09481e6d0fddd581a36c3bba61824218e8d0bcb2
pedrosh101 <pedro.sh101@gmail.com>
2022-10-16 22:51:57 -0300

    161022 commit

--- src/components/ProjectImg.js
+++ src/components/ProjectImg.js
@@ -1,65 +1,50 @@
 export const ProjectImg = [
   {
     id: 1,
-    name: "Beauty Spa",
-    aviso: "(em progresso)",
-    img: "../img/site1.jpg",
-    text: (
-      <a href="https://github.com/pedrosh101/beautyspa" target="blank">
-        github
+    name: (
+      <a href="https://peaceful-fudge-338096.netlify.app/" target="blank">
+        Beauty Spa
       </a>
     ),
+    aviso: "(em progresso)",
+    img: "../img/site1.jpg",
   },
   {
     id: 2,
-    name: "Eustaquio Neves",
-    aviso: "(em progresso)",
-    img: "../img/site2.jpg",
-    text: (
+    name: (
       <a href="https://resonant-torrone-ae6b29.netlify.app" target="blank">
-        netlify
+        Eustaquio Neves
       </a>
     ),
+    aviso: "(em progresso)",
+    img: "../img/site2.jpg",
   },
   {
     id: 3,
-    name: "Fisio Mockups",
-    img: "../img/site3.jpg",
-    text: (
-      <a href="https://proj3-fisio.web.app/" target="blank">
-        firebase1
+    name: (
+      <a href="https://luxury-wisp-2fbd9e.netlify.app/" target="blank">
+        Advocacia Mockup
       </a>
     ),
-
-    text2: (
-      <a href="https://proj-fisio4.web.app/" target="blank">
-        firebase2
-      </a>
-    ),
+    aviso:"",
+    img: "../img/site2.jpg",
   },
   {
     id: 4,
-    name: "Weather App",
-    img: "../img/site4.jpg",
-    text: (
-      <a href="https://proj1-weather.web.app/" target="blank">
-        firebase
+    name: (
+      <a href="https://voluble-platypus-a0a93f.netlify.app/" target="blank">
+        Academia Mockup
       </a>
     ),
-    text2: (
-      <a href="https://github.com/pedrosh101/proj1-weather" target="blank">
-        github
-      </a>
-    ),
+    img: "../img/site3.jpg",
   },
   {
     id: 5,
-    name: "Meu Portfolio",
-    img: "../img/site5.jpg",
-    text: (
-      <a href="https://github.com/pedrosh101/portfolio" target="blank">
-        github
+    name: (
+      <a href="https://proj1-weather.web.app/" target="blank">
+        Weather App
       </a>
     ),
+    img: "../img/site4.jpg",
   },
 ];
