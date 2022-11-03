9a6fe98aacccf6ff8a424fb060557ea7295ea43c
pedrosh101 <pedro.sh101@gmail.com>
2022-11-03 19:14:12 -0300

    0311 commit2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-
 import Main from "./components/Main";
 import Projects from "./components/Projects";
 import Invite from "./components/Invite";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
-
-
-
 function App() {
-
   return (
     <>
       <Main />
       <Projects />
-      <Invite
-        crossreveal={"yPercent"}
-      />
+      <Invite crossreveal={"yPercent"} />
       <Footer />
     </>
   );
