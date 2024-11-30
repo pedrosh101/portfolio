1c2304214e9956484ea43ba2d1c733ee65c8f003
pedrosh101 <pedro.sh101@gmail.com>
2024-11-30 09:47:32 -0300

    jpupdate1

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
 import close from "../svg/close.svg";
 
 function Modal({ open, onClose }) {
-  const { t } = useTranslation();
+
 
   if (!open) return null;
   return (
@@ -17,14 +16,12 @@ function Modal({ open, onClose }) {
         <div className="modalTop">
           <img src={close} alt="x" onClick={onClose} className="closeBtn" />
           <div className="contentModal">
-            <h2>{t("modal1")}</h2>
-            <p>{t("modal2")}</p>
             <h3>Web Development</h3>
             <p>
               HTML, CSS, JAVASCRIPT, TYPESCRIPT, REACT, NEXT, TAILWIND,
               BOOTSTRAP & WORDPRESS
             </p>
-            <h3>Web Design</h3>
+            <h2>Web Design</h2>
             <p>PHOTOSHOP, ILLUSTRATOR, FIGMA, CANVA & DAVINCI RESOLVE</p>
           </div>
         </div>
